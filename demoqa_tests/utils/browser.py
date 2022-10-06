5162beb0a0d1079fd52d71aeeebb7a1911a83d0d
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-10-06 16:22:39 +0400

    comments for assertions have been added

--- demoqa_tests/utils/browser.py
+++ demoqa_tests/utils/browser.py
@@ -1,6 +1,9 @@
 from selene import command
+from selene.core.wait import Command
+from selene.support.shared import browser
 
 from demoqa_tests.model.pages.registration_form import state_selector
+from demoqa_tests.utils.selene import command_ext
 
 
 def scroll_to_view():
@@ -8,4 +11,5 @@ def scroll_to_view():
 
 
 def scroll_one_page():
-    state_selector.perform(command.js.scroll_into_view)
+    browser.perform(command_ext.js.scroll_one_page)
+
