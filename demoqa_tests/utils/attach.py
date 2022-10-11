39aba1682c9489abff763e320b390ec69c337cd9
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-10-11 16:28:12 +0400

    names of funtion parameters were added in attach.py

--- demoqa_tests/utils/attach.py
+++ demoqa_tests/utils/attach.py
@@ -14,12 +14,22 @@ def add_screenshot(browser):
 
 def add_logs(browser):
     log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
-    allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
+    allure.attach(
+        body=log,
+        name='browser_logs',
+        attachment_type=AttachmentType.TEXT,
+        extension='.log',
+    )
 
 
 def add_html(browser):
     html = browser.driver.page_source
-    allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
+    allure.attach(
+        body=html,
+        name='page_source',
+        attachment_type=AttachmentType.HTML,
+        extension='.html',
+    )
 
 
 def add_video(browser):
