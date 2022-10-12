5a7d31088bfb9a6e6459452a8096791744ce4597
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-10-12 16:36:42 +0400

    conftest.py was changed

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,15 +13,15 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management(request):
+    browser_name = os.getenv('selene.browser_name', 'selenoid')
+    browser.config.window_width = 1000
+    browser.config.window_height = 1200
     browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
     # env -S 'selen.base_url = https://google.com' pytest tests/v1/test_practice_form.py
-    browser_name = os.getenv('selene.browser_name', 'selenoid')
     browser.config.hold_browser_open = bool(
         os.getenv('selene.hold_browser_open', 'false')
     )
     browser.config.timeout = float(os.getenv('selene.timeout', '3'))
-    browser.config.window_width = 1000
-    browser.config.window_height = 1200
 
     if browser_name != 'selenoid':
         browser.config.browser_name = browser_name
@@ -38,10 +38,10 @@ def browser_management(request):
             'selenoid:options': {'enableVNC': True, 'enableVideo': True},
         }
         options.capabilities.update(selenoid_capabilities)
-        browser.config.driver = webdriver.Remote(
+        driver = webdriver.Remote(
             command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
             options=options,
         )
-
-    yield browser
+        browser.config.driver = driver
+    yield
     browser.driver.close()
