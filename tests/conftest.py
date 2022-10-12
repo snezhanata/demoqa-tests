ec0d1b5b9884b6503277b4ffd97e23a35a6f06f4
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-10-12 20:27:05 +0400

    conftest.py updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,16 +11,16 @@ def pytest_addoption(parser):
     parser.addoption('--browser_version', default='99.0')
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def browser_management(request):
-    browser_name = os.getenv('selene.browser_name', 'selenoid')
+    browser_name = os.getenv('selene.browser_name', 'chrome')
     browser.config.window_width = 1000
     browser.config.window_height = 1200
     browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
     # env -S 'selen.base_url = https://google.com' pytest tests/v1/test_practice_form.py
-    browser.config.hold_browser_open = bool(
-        os.getenv('selene.hold_browser_open', 'true')
-    )
+    # browser.config.hold_browser_open = bool(
+    #     os.getenv('selene.hold_browser_open', 'True')
+    # )
     # browser.config.timeout = float(os.getenv('selene.timeout', '3'))
 
     if browser_name != 'selenoid':
