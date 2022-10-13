7b864d8610e5eace8468a85ce77ab6033fa10aa0
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-10-14 00:18:21 +0400

    timeout was increased

--- demoqa_tests/model/google.py
+++ demoqa_tests/model/google.py
@@ -8,5 +8,7 @@ def ads_remove(*, amount, timeout):
     # ads.with_(timeout=timeout).should(have.size_greater_than_or_equal(amount)).perform(
     #     command.js.remove
     # )
-    if ads.with_(timeout=timeout).wait_until(have.size_greater_than_or_equal(amount)):
+    if ads.with_(timeout=timeout).wait_until(
+        have.size_greater_than_or_equal(amount=amount)
+    ):
         ads.perform(command.js.remove)
