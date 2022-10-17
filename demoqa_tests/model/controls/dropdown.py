9d88f4eb6bd0499f7a214b660155db6e011f7c49
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-10-17 19:30:49 +0400

    classes for controls were added

--- demoqa_tests/model/controls/dropdown.py
+++ demoqa_tests/model/controls/dropdown.py
@@ -1,5 +1,5 @@
 import selene
-from selene import have
+from selene import have, command
 from selene.support.shared import browser
 
 
@@ -7,8 +7,13 @@ class Dropdown:
     def __init__(self, element: selene.Element):
         self.element = element
 
+    def scroll_to_view(self):
+        self.element.perform(command.js.scroll_into_view)
+        return self
+
     def select(self, option):
         self.element.click()
         browser.all('[id^=react-select][id*=-option-]').by(
             have.exact_text(option)
         ).first.click()  # react-select-3-option-2
+        return self
--- demoqa_tests/model/controls/radio_button.py
+++ demoqa_tests/model/controls/radio_button.py
@@ -5,9 +5,12 @@ from selene.support.shared import browser
 from tests.test_data.users import user, Gender
 
 
-def option(element: selene.Element, value: str):
-    element.by(have.exact_text(value)).first.click()  # noqa
+class RadioButton:
+    def __init__(self, element: selene.Element):
+        self.element = element
 
+    def option(self, value: str):
+        self.element.by(have.exact_text(value)).first.click()  # noqa
 
-# def set_option(label_prefix: str, number: int):
-#     browser.element(f'[for={label_prefix}-radio-{number}]').click()
+    # def set_option(label_prefix: str, number: int):
+    #     browser.element(f'[for={label_prefix}-radio-{number}]').click()
