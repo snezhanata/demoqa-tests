6fa996b19fe27d3fd7aaa0ce97de8238021e936d
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-10-17 19:04:19 +0400

    changes related to oop

--- demoqa_tests/model/controls/dropdown.py
+++ demoqa_tests/model/controls/dropdown.py
@@ -1,18 +1,14 @@
-from selene import have, command
+import selene
+from selene import have
 from selene.support.shared import browser
 
 
-def select(element, option):
-    element.click()
-    browser.all('[id^=react-select][id*=-option-]').by(
-        have.exact_text(option)
-    ).first.click()  # react-select-3-option-2
+class Dropdown:
+    def __init__(self, element: selene.Element):
+        self.element = element
 
-
-# def select(element, option, autocomplete: bool = False):
-# def autocomplete(selector: str, from_: str, to: str = None):
-#     browser.element(selector).click().type(from_)
-#     if to is not None:
-#         browser.element('#state').element_by(have.text(to)).perform(command.js.click)
-#     else:
-#         browser.element('#state').element_by(have.text(from_)).perform(command.js.click)
+    def select(self, option):
+        self.element.click()
+        browser.all('[id^=react-select][id*=-option-]').by(
+            have.exact_text(option)
+        ).first.click()  # react-select-3-option-2
--- demoqa_tests/model/pages/registration_form.py
+++ demoqa_tests/model/pages/registration_form.py
@@ -5,9 +5,10 @@ from selene.support.shared import browser
 
 from demoqa_tests import utils
 
-from demoqa_tests.model.controls import dropdown, radio_button, checkbox
+from demoqa_tests.model.controls import radio_button, checkbox
 from demoqa_tests.model import google
 from demoqa_tests.model.controls.datepicker import DatePicker
+from demoqa_tests.model.controls.dropdown import Dropdown
 from demoqa_tests.model.pages.submission_form import SubmissionForm
 from demoqa_tests.utils import path
 from tests.test_data.users import Subject, Hobby
@@ -15,9 +16,9 @@ from tests.test_data.users import Subject, Hobby
 
 class RegistrationForm:
     def __init__(self):
-        self.state_selector = browser.element('#state')
-        self.city_selector = browser.element('#city')
-        self.birthday = DatePicker(browser.element('#dateOfBirthInput'))
+        self.state_selector = Dropdown(browser.element('#state'))
+        self.city_selector = Dropdown(browser.element('#city'))
+        self.birthday_selector = DatePicker(browser.element('#dateOfBirthInput'))
 
     def open(self):
         browser.open('/automation-practice-form')
@@ -39,16 +40,26 @@ class RegistrationForm:
         return self
 
     def fill_date(self, value: datetime.date):
-        self.birthday.set_date(value)
+        self.birthday_selector.set_date(value)
         return self
 
     def assert_fill_date(self, value: datetime.date):
         # self.birthday.should(match.date(value))  # проверка по Мартину Фаулеру
-        self.birthday.assert_value(value)  # проверка НЕ по Мартину Фаулеру
+        self.birthday_selector.assert_value(value)  # проверка НЕ по Мартину Фаулеру
         return self
 
     def select_date(self, day: int, month: str, year: int):
-        self.birthday.calendar(day, month, year)
+        self.birthday_selector.calendar(day, month, year)
+        return self
+
+    def select_state(self, value: str):
+        utils.browser.scroll_to_view(self.state_selector)
+        # utils.browser.scroll_one_page()
+        self.state_selector.select(value)
+        return self
+
+    def select_city(self, value: str):
+        self.city_selector.select(value)
         return self
 
     def fill_subjects(self, values: Tuple[Subject]):
@@ -79,16 +90,6 @@ class RegistrationForm:
 
     def fill_address(self, value: str):
         browser.element('#currentAddress').type(value)
-        return self
-
-    def select_state(self, value: str):
-        utils.browser.scroll_to_view(self.state_selector)
-        # utils.browser.scroll_one_page()
-        dropdown.select(self.state_selector, value)
-        return self
-
-    def select_city(self, value: str):
-        dropdown.select(self.city_selector, value)
         return self
 
     def submit(self):
